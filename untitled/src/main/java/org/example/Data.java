c806ef45326ca785a0dab77c0f2ee0e948d34a54
yanivcohen223 <yanivcohen223@gmail.com>
2023-03-11 10:53:18 +0200

    upload

---
+++ untitled/src/main/java/org/example/Data.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Data <T extends Number>{
+    public T getRandNum() {
+        return null;
+    }
+
+    int RandNum;
+    public Data(int randNum) {
+    }
+}
